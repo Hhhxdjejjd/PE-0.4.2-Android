b7fbd2808173ef230f4c50e48b858ea3799ec7a7
Hhhxdjejjd <99557090+Hhhxdjejjd@users.noreply.github.com>
2022-04-01 15:14:05 -0400

    Hdbdhx
    Dhdhdg

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -100,7 +100,7 @@ class StoryMenuState extends MusicBeatState
 			weekThing.targetY = i;
 			grpWeekText.add(weekThing);
 
-			weekThing.screenCenter(X);
+			weekThing.x = 100;
 			weekThing.antialiasing = ClientPrefs.globalAntialiasing;
 			// weekThing.updateHitbox();
 
